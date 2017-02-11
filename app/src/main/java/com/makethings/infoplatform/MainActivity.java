7320664449202ef96bc500b14eda319564eb91f3
xiongxiongxiong8718 <915968268@qq.com>
2017-02-11 20:25:09 +0800

    9 13 14completing

--- app/src/main/java/com/makethings/infoplatform/MainActivity.java
+++ app/src/main/java/com/makethings/infoplatform/MainActivity.java
@@ -1,13 +1,40 @@
 package com.makethings.infoplatform;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button button9,button13,button14;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button9= (Button) findViewById(R.id.btn_test9);
+        button9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,MineClubActivity.class);
+                startActivity(intent);
+            }
+        });
+        button13= (Button) findViewById(R.id.btn_test13);
+        button13.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,ModifyClubDataActivity.class);
+                startActivity(intent);
+            }
+        });
+        button14= (Button) findViewById(R.id.btn_test14);
+        button14.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,MineClubMembersActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/makethings/infoplatform/MineClubActivity.java
+++ app/src/main/java/com/makethings/infoplatform/MineClubActivity.java
@@ -1,8 +1,30 @@
 package com.makethings.infoplatform;
 
-/**
- * Created by Call Me Bear on 2017/2/10.
- */
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TabHost;
 
-public class MineClubActivity {
+public class MineClubActivity extends AppCompatActivity {
+
+    private String[] data={"1","2","3","4","5","6","7","8","9","10","11","12","13","14"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mine_club);
+
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(MineClubActivity.this,android.R.layout.simple_list_item_1,data);
+        ListView listJoin= (ListView) findViewById(R.id.mine_club_join_list);
+        ListView listManager= (ListView) findViewById(R.id.mine_club_manager_list);
+        listJoin.setAdapter(adapter);
+        listManager.setAdapter(adapter);
+
+        //初始化TabHost
+        TabHost tabHost= (TabHost) findViewById(R.id.tabhost);
+        tabHost.setup();
+        tabHost.addTab(tabHost.newTabSpec("tab_join").setIndicator("我加入的社团").setContent(R.id.tab_mine_club_join));
+        tabHost.addTab(tabHost.newTabSpec("tab_manager").setIndicator("我管理的社团").setContent(R.id.tab_mine_club_manager));
+    }
 }
